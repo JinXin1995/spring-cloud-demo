f22661190dce66fb9af437af3ef7bb00412be621
金鑫 <javen_king@qq.com>
2022-06-29 12:49:49 +0800

    init

---
+++ eureka-client/src/test/java/lpwa/cloud/app/eurekaclient/EurekaClientApplicationTests.java
@@ -0,0 +1,13 @@
+package lpwa.cloud.app.eurekaclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
