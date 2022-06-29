da6647ae50bea931781b93ef629c15a81e715fdb
金鑫 <javen_king@qq.com>
2022-06-29 15:07:43 +0800

    服务熔断 Ribbon

--- service-ribbon/src/main/java/lpwa/cloud/app/serviceribbon/service/impl/TestServiceImpl.java
+++ service-ribbon/src/main/java/lpwa/cloud/app/serviceribbon/service/impl/TestServiceImpl.java
@@ -1,5 +1,6 @@
 package lpwa.cloud.app.serviceribbon.service.impl;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import lpwa.cloud.app.serviceribbon.service.TestService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,7 +13,12 @@ public class TestServiceImpl implements TestService {
     private RestTemplate restTemplate;
 
     @Override
+    @HystrixCommand(fallbackMethod = "error")
     public String test(String name) {
         return restTemplate.getForObject("http://EUREKA-CLIENT/test?name=" + name, String.class);
+    }
+
+    public String error(String name) {
+        return name + " error!";
     }
 }
